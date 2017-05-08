641b00ecfd39e8dc405b3177a4af5b5b57353a0a
juansebastianvega <sebastian_vega@yahoo.com>
2017-05-08 15:10:27 -0400

    add otra
    I added another page, called "otra"
    also removed the _blank attrib

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    /* This is the 1st. version of the Index.php
+    *  Author: S.Vega
+    *  Date 5/8/2017
+    */
+
+    echo "<h1>The date is ".date("m-d-Y")."</h1><br><br>";
+?>
+    <a href="otra.php">Otra Pagina</a>
+    <a href="http://www.yahoo.com" target="_blank">Yahoo!</a>
+<?php
+    exit();
+?>
